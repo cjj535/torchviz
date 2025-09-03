c94e62ebe185c4ec95cf377e1b031c787de64ee4
cjj535 <767876863@qq.com>
2025-09-03 22:52:30 +0800

    [FIX] add edge in main graph

--- core/json_to_complex_dot.py
+++ core/json_to_complex_dot.py
@@ -137,53 +137,28 @@ def json_to_complex_dot(graph_data: List[Dict], tree_data: List[Dict]) -> str:
                 tensor_map[tensor_key] = TensorInfo(tensor)
             tensor_map[tensor_key].producer = node_id
 
-    # 3.推导tensor属于哪个子图中，tensor属于某一个子图即tensor的所有生产者和消费者均在该子图中
-    def find_subgraph_id(producer_id: int, comsumer_ids: List[int]) -> int:
-        # 与tensor相关联的op仅有一个（数据保证tensor必有关联的op）
-        if producer_id == virtual_root_id or comsumer_ids == []:
+    # 3.推导op属于哪个子图中
+    def get_subgraph_id(op_id: int) -> int:
+        # 无生产者
+        if op_id == virtual_root_id:
             return virtual_root_id
 
-        # 合并生产者消费者，合并后数量至少为2
-        op_ids: List[int] = [producer_id] + comsumer_ids
-
-        def get_ancestors_id(op_id: int) -> List[int]:
-            ancestors = []
-            while True:
-                ancestors.append(op_id)
-                if node_map[op_id]["parent"]:
-                    op_id = node_map[op_id]["parent"]
-                else:
-                    break
-            return ancestors
-
-        # 生成第一个节点的祖先链
-        ancestors = get_ancestors_id(op_ids[0])
-        ancestor_set = set(ancestors)
-
-        # 对剩余节点，取祖先交集
-        for op_id in op_ids[1:]:
-            ancestor_set &= set(get_ancestors_id(op_id))
-
-        # 从第一个节点的祖先链（自底向上）找第一个在交集里的
-        for a in ancestors:
-            if a in ancestor_set:
-                return a
-
-        # 无共同祖先
-        return virtual_root_id
+        # 获取生产者的父节点id
+        assert node_map[op_id]["parent"] is not None
+        return node_map[op_id]["parent"]
 
     tensors_in_subgraph: Dict[int, List[str]] = {}
     for tensor_key, tensor_info in tensor_map.items():
-        subgraph_id = find_subgraph_id(tensor_info.producer, tensor_info.comsumers)
+        subgraph_id = get_subgraph_id(tensor_info.producer)         # 生产者决定tensor属于哪个子图
         if subgraph_id in tensors_in_subgraph:
             tensors_in_subgraph[subgraph_id].append(tensor_key)
         else:
             tensors_in_subgraph[subgraph_id] = [tensor_key]
 
-    # 4.dfs遍历树并生成str
+    # 4.dfs遍历树并分析从属关系
     dot_lines: List[str] = []
 
-    # dfs遍历函数，每次调用处理一个子图
+    # dfs遍历函数，每次调用分析一个子图中应有的节点，包括tensor节点、op节点、子图
     def dfs(root_id: int, children: List[int], depth: int, ) -> List[str]:
         sub_dot_lines: List[str] = []
         for node_id in children:
@@ -197,34 +172,36 @@ def json_to_complex_dot(graph_data: List[Dict], tree_data: List[Dict]) -> str:
                 #     sub_dot_lines.append(f'{"    "*(depth+1)}rankdir=LR;')
                 # elif node_map[node_id]["scope"] == "backward":
                 #     sub_dot_lines.append(f'{"    "*(depth+1)}rankdir=LR;')
-                # elif node_map[node_id]["scope"] == "postprocess":
-                #     sub_dot_lines.append(f'{"    "*(depth+1)}rankdir=LR;')
                 sub_dot_lines.append(f'{"    "*(depth+1)}label="{node_map[node_id]["name"]}";')
                 sub_dot_lines.append(f'{"    "*(depth+1)}style=rounded;')
                 sub_dot_lines.append(f'{"    "*(depth+1)}color=blue;')
                 sub_dot_lines += dfs(node_id, node_map[node_id]["children"], depth+1)
                 sub_dot_lines.append(f'{"    "*depth}}}')
 
-        # 添加tensor及其连线
+        # 添加tensor节点
         for tensor_key in tensors_in_subgraph.get(root_id, []):
             sub_dot_lines.append(f'{"    "*depth}"tensor_{tensor_key}" [label="{tensor_map[tensor_key].label}", shape=ellipse];')
-            producer_id = tensor_map[tensor_key].producer
-            if producer_id != virtual_root_id:
-                sub_dot_lines.append(f'{"    "*depth}"node_{producer_id}" -> "tensor_{tensor_key}";')
-            for comsumer_id in tensor_map[tensor_key].comsumers:
-                sub_dot_lines.append(f'{"    "*depth}"tensor_{tensor_key}" -> "node_{comsumer_id}";')
 
         return sub_dot_lines
 
     root_nodes = [k for k, v in node_map.items() if v["parent"] is None]
     dot_lines = dfs(virtual_root_id, root_nodes, depth=1)
 
-    # 5.定义dot文件头尾，完成组装
+    # 5.添加边
+    for subgraph_id, tensor_key_list in tensors_in_subgraph.items():
+        for tensor_key in tensor_key_list:
+            producer_id = tensor_map[tensor_key].producer
+            if producer_id != virtual_root_id:
+                dot_lines.append(f'{"    "}"node_{producer_id}" -> "tensor_{tensor_key}";')
+            for comsumer_id in tensor_map[tensor_key].comsumers:
+                dot_lines.append(f'{"    "}"tensor_{tensor_key}" -> "node_{comsumer_id}";')
+
+    # 6.定义dot文件头尾，完成组装
     root_dot_lines: List[str] = []
     root_dot_lines.append("digraph G {")
-    root_dot_lines.append('  rankdir=LR;')  # 从左到右绘制
-    root_dot_lines.append('  node [fontname="Arial"];')
+    root_dot_lines.append('    rankdir=LR;')              # 从左到右绘制
+    root_dot_lines.append('    node [fontname="Arial"];')
     root_dot_lines.append("}")
-    result = "\n".join(root_dot_lines[:3] + dot_lines + root_dot_lines[3:])
+    result = "\n".join(root_dot_lines[:-1] + dot_lines + root_dot_lines[-1:])
 
     return result
